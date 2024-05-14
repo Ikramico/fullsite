8475300bc6db8f56b739795efc73e0c9fe2f600d
Ikram Ico <ikramicono@gmail.com>
2024-05-14 11:51:37 +0600

    cart complete

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 let products = [
 	{
-		title: 'Forest Art', 
+		id: 1,
+		title: "Forest Art",
 		price: 200,
-		image: './images/p1.jpg'
+		image: "./images/p1.jpg",
 	},
 	{
-		title: 'Plain Art', 
+		id: 2,
+		title: "Plain Art",
 		price: 500,
-		image: './images/p2.jpg'
+		image: "./images/p2.jpg",
 	},
 	{
-		title: 'Manuver', 
+		id: 3,
+		title: "Manuver",
 		price: 700,
-		image: './images/p3.jpg'
+		image: "./images/p3.jpg",
 	},
 	{
-		title: 'Cas Art', 
+		id: 4,
+		title: "Cas Art",
 		price: 400,
-		image: './images/p4.jpg'
+		image: "./images/p4.jpg",
 	},
 	{
-		title: 'Breeze', 
+		id:5,
+		title: "Breeze",
 		price: 500,
-		image: './images/p5.jpg'
-	}
-	
-]
-
+		image: "./images/p5.jpg",
+	},
+];
+let cartItem =[];
 
 
 let hamBurger = document.querySelector('.icon');
 let navItems = document.querySelector('.navitems');
 /**Catch Product Div */
 let productDiv = document.querySelector('.products');
+/**Cart */
+let showCart = document.querySelector('.cart');
+let total = document.querySelector('.subtotal');
 
 function showNav(){
 navItems.classList.toggle("hidden");
@@ -70,7 +77,38 @@ products.forEach((item)=>{
             <img src="${item.image}" class="product-image">
             <p class="title text-sm font-bold text-center">${item.title}</p>
             <p class="price text-center">${item.price}</p>
-            <button type="button" class="text-center bg-gray-50 p-1 rounded-lg shadow-lg shadow-slate-800">Add to cart</button>
+            <button type="button" class="text-center bg-gray-50 p-1 rounded-lg shadow-lg shadow-slate-800" onclick='cart(${item.id})'>Add to cart</button>
         </div>`;
-	// document.createElement('div')
-})
+});
+
+function cart(i){
+	console.log(i);
+if(!cartItem.includes(i)){
+cartItem.push(i);
+}
+
+let price =0;
+showCart.innerHTML ='';
+ cartItem.forEach((item) => {
+	
+	let product = products[item - 1];
+	console.log(product);
+	
+	price += product.price
+	showCart.innerHTML += `
+	<div class="product flex flex-col justify-center">
+            <img src="${product.image}" class="product-image h-[10vh]">
+            <p class="title text-sm font-bold text-center">${product.title}</p>
+            <p class="price text-center">${product.price}</p>
+            
+        </div>`;
+
+		total.innerHTML = ` Subtotal: ${price}`
+		
+});
+
+
+}
+
+
+
